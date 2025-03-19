64df4984bac6d6743f5dc853278473cda8abbe2d
meti2626 <metseboka@gmail.com>
2025-03-19 17:00:51 +0300

    Create main.jsx

---
+++ Week-5/src/main.jsx
@@ -0,0 +1,19 @@
+ul {
+    list-style: none;
+    display: flex;
+    gap: 8px;
+
+
+}
+
+button {
+    color: bisque;
+    background-color: blueviolet;
+    border: none;
+    border-radius: 10px;
+    padding: 4px 6px;
+    
+}
+button:hover{
+background-color: rgb(171, 122, 216);
+}
